5bc0d4e28ff020c91706dbb93441b779f491b6a4
@11808s8 <adrianogsss@gmail.com>
2019-06-13 18:46:03 -0300

    Adição do bootstrap no wagtail e página home_page para teste!

--- financialeducation/home/models.py
+++ financialeducation/home/models.py
@@ -5,8 +5,11 @@ from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
-    ]
+    max_count = 1
+    template = 'home/home_page.html'
+    # body = RichTextField(blank=True)
+
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('body', classname="full"),
+    # ]
