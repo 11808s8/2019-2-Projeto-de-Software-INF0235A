625c9ec58ecbb41b87a02e77ec1cb83980b9b044
Adriano Gomes <adrianogsss@gmail.com>
2019-06-13 20:15:02 -0300

    Adição de javascript para a página principal

---
+++ financialeducation/home/static/js/home_page.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#btn-1').on('click', function(){
+        alert("Bruno");
+    });
+    $('#btn-2').on('click', function(){
+        alert("Ultra gay");
+    });
+});
