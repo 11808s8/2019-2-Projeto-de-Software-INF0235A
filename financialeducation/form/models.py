4560fca95f07b25766908fdde2b9a15cd8ac43dd
Adriano Gomes <adrianogsss@gmail.com>
2019-06-16 23:35:16 -0300

    Alteração do form ajustada pq o leopoldo nao fez

--- financialeducation/form/models.py
+++ financialeducation/form/models.py
@@ -6,24 +6,4 @@ from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.search import index
 
 class FormIndexPage(Page):
-    intro = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
-
-class FormPage(Page):
-    date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
-
-    search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
-    ]
-
-    content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
-    ]
+    template = "form/form_index_page.html"
