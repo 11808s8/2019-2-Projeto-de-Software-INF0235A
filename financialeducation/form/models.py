a790b4e71abece7230fabad0e8b24d0db4ef07e1
Adriano Gomes <adrianogsss@gmail.com>
2019-06-27 12:46:48 -0300

    Adição de snippet de categoria

--- financialeducation/form/models.py
+++ financialeducation/form/models.py
@@ -5,5 +5,28 @@ from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.search import index
 
+from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+
+
+@register_snippet
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    category_type = models.CharField(max_length=255)
+    # slug = models.SlugField(unique=True, max_length=80)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('category_type'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
 class FormIndexPage(Page):
     template = "form/form_index_page.html"
+    
